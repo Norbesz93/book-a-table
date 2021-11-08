e00169d5b3f9cdc65180838c8a0b07fd1baab6de
gaborsimonits <gaborsimonits@gmail.com>
2021-11-08 18:51:28 +0100

    logochange

--- app.jsx
+++ app.jsx
@@ -33,13 +33,13 @@ const booking = () => {
 const App = () => {
     return (
         <div>
-            <img id="logo" src="/pizzahut.jpeg" alt="pizza hut logo"/>
+            <img id="logo" src="/hotpizza.jpg" alt="pizza hut logo"/>
             
             <nav id="nav">
                 <div id="intro"> 
                     <p id="text">
-                        Pizza Hut is an American multinational restaurant chain and international franchise founded in 1958 in Wichita, Kansas by Dan and Frank Carney. It provides pizza and other Italian-American dishes, including pasta, side dishes and desserts.
-                        The chain has 18,703 restaurants worldwide as of December 31, 2019,[2] making it the world's largest pizza chain in the number of locations.
+                        Hot Pizza Shutterstock is an American multinational restaurant chain and international franchise founded in 1958 in Wichita, Kansas by Dan and Frank Carney. It provides pizza and other Italian-American dishes, including pasta, side dishes and desserts.
+                        The chain has 18,703 restaurants worldwide as of December 31, 2019, making it the world's largest pizza chain in the number of locations.
                         It is a subsidiary of Yum! Brands, Inc., one of the world's largest restaurant companies.
                     </p>
                 </div>
